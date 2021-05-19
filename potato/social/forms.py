8117250ddc77e9912243edda52146c36e8470eb9
Sreedhar k.s <53270409+mellofordev@users.noreply.github.com>
2021-05-19 19:40:40 +0530

    Updated forms.py

--- potato/social/forms.py
+++ potato/social/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post,Explore,Comment
+from .models import Post,Explore,Comment,Chat,Quickpost
 from django.contrib.auth.models import User
 
 
@@ -7,7 +7,7 @@ class Posting(forms.ModelForm):
       class Meta:
           model =Post
 
-          fields = ('post','pic','hashtag','typ')
+          fields = ('post','pic')
 class Exploreform(forms.ModelForm):
     class Meta:
         model=Explore
@@ -16,3 +16,19 @@ class Comment(forms.ModelForm):
     class Meta:
         model=Comment
         fields=('comment',)
+class Facemashing(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields=('image1','image2')
+class Youtubeing(forms.ModelForm):
+    class Meta:
+        model=Post
+        fields=('post','src',)
+class Chating(forms.ModelForm):
+    class Meta:
+        model=Chat
+        fields=('text',)
+class Quickposting(forms.ModelForm):
+    class Meta:
+        model=Quickpost
+        fields=('quickpost',)
