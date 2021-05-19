7bb1b1a9d7e55bb55efbdd29357a5f1b58a22a95
Sreedhar k.s <53270409+mellofordev@users.noreply.github.com>
2021-05-19 19:44:40 +0530

    Updated accounts/forms.py

--- potato/accounts/forms.py
+++ potato/accounts/forms.py
@@ -3,15 +3,38 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
+
+locker=[('on','ON'),('off','OFF')]
+
 class SignUpForm(UserCreationForm):
     name = forms.CharField(max_length=30,required=True)
-
+    year = ['2003']
+    #born = forms.DateField(label='Date of birth', widget=forms.SelectDateWidget(years=year))
     class Meta:
         model = User
-        fields = ('username', 'name', 'password1', 'password2', )
+        fields = ('username','email', 'name', 'password1', 'password2' )
 
 
 class ProfileForm(forms.ModelForm):
+    year = ['2003']
+    born = forms.DateField(label='Date of birth', widget=forms.SelectDateWidget(years=year))
+    lockprofile =forms.CharField(label='Private account?',initial='off', widget=forms.Select(choices=locker))
     class Meta:
         model = Profile
-        fields = ('user', 'bio', 'name','pic')
+        fields = ('bio', 'name','pic','status','born')
+class EmailForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields =('email',)
+class ResetForm(forms.ModelForm):
+    emailreset=forms.CharField(max_length=100,required=True)
+
+    class Meta:
+        model=Profile
+        fields=('emailreset',)
+class PasswordresetForm(forms.ModelForm):
+    newpassword=forms.CharField(max_length=32, widget=forms.PasswordInput)
+    confirmpassword=forms.CharField(max_length=32, widget=forms.PasswordInput)
+    class Meta:
+        model=User
+        fields=('newpassword','confirmpassword')
